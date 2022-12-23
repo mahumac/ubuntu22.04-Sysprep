58f7c86d114f2a94efb0efc280c3f4ae48553862
netwerkfix <67594538+netwerkfix@users.noreply.github.com>
2022-12-23 08:33:58 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+######################################################
+#### WARNING PIPING TO BASH IS STUPID: DO NOT USE THIS
+######################################################
+# modified from: jcppkkk/prepare-ubuntu-template.sh
+# TESTED ON UBUNTU 18.04 LTS (Alternate installer, without cloud-init)
+# TESTED ON UBUNTU 22.04 LTS (Works for me)
+# http://cdimage.ubuntu.com/releases/18.04.2/release/ubuntu-18.04.2-server-amd64.iso
+
+# SETUP & RUN
+# curl -sL https://raw.githubusercontent.com/netwerkfix/linux-virt-sysprep/master/prepare-ubuntu-18.04-template.sh | sudo -E bash -
+
+if [ `id -u` -ne 0 ]; then
+	echo Need sudo
+	exit 1
+fi
+
+set -v
+
+#update apt-cache
+apt update -y
+apt upgrade -y
+
+#install packages
+apt install -y open-vm-tools
+
+#Stop services for cleanup
+service rsyslog stop
+
+#clear audit logs
+if [ -f /var/log/wtmp ]; then
+    truncate -s0 /var/log/wtmp
+fi
+if [ -f /var/log/lastlog ]; then
+    truncate -s0 /var/log/lastlog
+fi
+
+#cleanup /tmp directories
+rm -rf /tmp/*
+rm -rf /var/tmp/*
+
+#cleanup current ssh keys
+rm -f /etc/ssh/ssh_host_*
+
+#add check for ssh keys on reboot...regenerate if neccessary
+cat << 'EOL' | sudo tee /etc/rc.local
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+# dynamically create hostname (optional)
+if hostname | grep localhost; then
+    hostnamectl set-hostname "$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')"
+fi
+
+test -f /etc/ssh/ssh_host_dsa_key || dpkg-reconfigure openssh-server
+exit 0
+EOL
+
+# make sure the script is executable
+chmod +x /etc/rc.local
+
+#reset hostname
+truncate -s0 /etc/hostname
+hostnamectl set-hostname localhost
+
+#reset machine-id
+truncate -s0 /etc/machine-id
+
+#cleanup apt
+apt clean
+
+# disable swap
+sudo swapoff --all
+sudo sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
+
+#cleanup shell history
+cat /dev/null > ~/.bash_history && history -c
+history -w
+
+#disk biger-maker (proxmox virtial disk)
+pvresize /dev/vda3
+lvextend -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
+resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
+
+#Shutdown in 2min
+shutdown -h +2
+
+#stop de link
+systemctl disable --now sysprep.service
+
