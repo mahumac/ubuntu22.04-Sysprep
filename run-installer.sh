031908e4a9cddfa6d15e77d20d587fd52b351560
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-25 21:45:59 +0100

    Update run-installer.sh

--- run-installer.sh
+++ run-installer.sh
@@ -1,10 +1,9 @@
 mkdir -p /etc/sysprep/
 cd /etc/sysprep/
-wget https://github.com/netwerkfix/ubuntu22.04-Sysprep/blob/main/run.sh
-wget https://github.com/netwerkfix/ubuntu22.04-Sysprep/blob/main/delay-boot.sh
+wget https://raw.githubusercontent.com/netwerkfix/ubuntu22.04-Sysprep/main/run.sh
 
 cd /etc/systemd/system/
-wget https://github.com/netwerkfix/ubuntu22.04-Sysprep/blob/main/sysprep.service
+wget https://raw.githubusercontent.com/netwerkfix/ubuntu22.04-Sysprep/main/sysprep.service
 
 chmod 755 /etc/systemd/system/sysprep.service
 chmod 755 /etc/sysprep/run.sh
