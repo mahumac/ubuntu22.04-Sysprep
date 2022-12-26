59bd813443d052c85fbfa6e872ea2e984a1f09d4
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 05:41:50 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,9 +2,9 @@
 ######################################################
 #### WARNING PIPING TO BASH IS STUPID: DO NOT USE THIS
 ######################################################
-# modified from: jcppkkk/prepare-ubuntu-template.sh
-# TESTED ON UBUNTU 18.04 LTS (Alternate installer, without cloud-init)
 # TESTED ON UBUNTU 22.04.1 LTS (Works for me)
+
+# this command give the script sudo perms
 
 if [ `id -u` -ne 0 ]; then
 	echo Need sudo
@@ -13,6 +13,30 @@ fi
 
 set -v
 
+# making working dir
+mkdir -p /etc/sysprep/
 
+# chance dir
+cd /etc/sysprep/
+
+# get files
+wget https://raw.githubusercontent.com/netwerkfix/ubuntu22.04-Sysprep/main/preping.sh
+
+# give it perms
+chmod 755 /etc/sysprep/preping.sh
+
+# then we need make service
+# chance the dir for the service
+cd /etc/systemd/system/
+
+# get files again
+wget https://raw.githubusercontent.com/netwerkfix/ubuntu22.04-Sysprep/main/sysprep.service
+
+# now we give it perms
+chmod 755 /etc/systemd/system/sysprep.service
+
+# sleep is a command that allow you give it delay in sec of 3sec
 sleep 3
+
+# now we enable the service
 systemctl enable --now sysprep.service
