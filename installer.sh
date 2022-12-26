4cd1fef64e45825a64aa2fd6a78094d3de141afc
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 07:45:05 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -44,6 +44,8 @@ chmod 755 /etc/systemd/system/updates.service
 systemctl enable sysprep.service
 systemctl enable updates.service
 
+sleep 380
+
 # now we enable the service
 systemctl disable sysprep.service
 systemctl disable updates.service
