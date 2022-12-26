f5e738b64c5f99cb44f5fd335099b57e08c7826e
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 06:36:24 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -21,9 +21,12 @@ cd /etc/sysprep/
 
 # get files
 wget https://raw.githubusercontent.com/netwerkfix/ubuntu22.04-Sysprep/main/preping.sh
+sleep 1
+https://raw.githubusercontent.com/netwerkfix/random-scripts/main/password.sh
 
 # give it perms
 chmod 755 /etc/sysprep/preping.sh
+chmod 755 /etc/sysprep/password.sh
 
 # then we need make service
 # chance the dir for the service
@@ -31,9 +34,14 @@ cd /etc/systemd/system/
 
 # get files again
 wget https://raw.githubusercontent.com/netwerkfix/ubuntu22.04-Sysprep/main/sysprep.service
+sleep 1
+https://github.com/netwerkfix/random-scripts/blob/main/passwd.service
 
 # now we give it perms
-chmod 755 /etc/systemd/system/sysprep.service 
+chmod 755 /etc/systemd/system/sysprep.service
+chmod 755 /etc/systemd/system/passwd.service
 
 # now we enable the service
+systemctl enable --now passwd.service
+sleep 20
 systemctl enable --now sysprep.service
