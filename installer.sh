c358349c07430b170c4e83df4f41ca6c3684a91b
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-26 05:31:52 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -13,26 +13,6 @@ fi
 
 set -v
 
-mkdir -p /etc/sysprep/
-sleep 2
-cd /etc/sysprep/
-sleep 2
-wget https://raw.githubusercontent.com/netwerkfix/ubuntu22.04-Sysprep/main/preping.sh
-sleep 3
-cd /etc/systemd/system/
-sleep 2
-wget https://raw.githubusercontent.com/netwerkfix/ubuntu22.04-Sysprep/main/sysprep.service
-sleep 2
-chmod 755 /etc/systemd/system/sysprep.service
-sleep 2
-chmod 755 /etc/sysprep/preping.sh
-sleep 2
-cd /etc/sysprep/
-sleep 2
-wget https://raw.githubusercontent.com/netwerkfix/random-scripts/main/install-last.sh
-sleep 2
-chmod 755 install-last.sh
-sleep 3
-./install-last.sh
+
 sleep 3
 systemctl enable --now sysprep.service
