54cbef04520b50822018639f9e1d50367881b8b0
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-27 08:07:24 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -29,55 +29,16 @@ sleep 10
 apt update -y
 apt upgrade -y
 
-apt install pwgen
-
 #install packages
+apt install pwgen
+sleep 1
 apt install -y qemu-guest-agent
-
-#Stop services for cleanup
-service rsyslog stop
-
-#clear audit logs
-if [ -f /var/log/wtmp ]; then
-    truncate -s0 /var/log/wtmp
-fi
-if [ -f /var/log/lastlog ]; then
-    truncate -s0 /var/log/lastlog
-fi
-
-#cleanup /tmp directories
-rm -rf /tmp/*
-rm -rf /var/tmp/*
-
-#cleanup current ssh keys
-rm -f /etc/ssh/ssh_host_*
-
-#add check for ssh keys on reboot...regenerate if neccessary
-cat << 'EOL' | sudo tee /etc/rc.local
-#!/bin/sh -e
-#
-# rc.local
-#
-# This script is executed at the end of each multiuser runlevel.
-# Make sure that the script will "" on success or any other
-# value on error.
-#
-# In order to enable or disable this script just change the execution
-# bits.
-#
-# By default this script does nothing.
-
-# dynamically create hostname (optional)
-if hostname | grep localhost; then
-    hostnamectl set-hostname "$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')"
-fi
-
-test -f /etc/ssh/ssh_host_dsa_key || dpkg-reconfigure openssh-server
-exit 0
-EOL
-
-# make sure the script is executable
-chmod +x /etc/rc.local
+sleep 1
+/bin/rm -v /etc/ssh/ssh_host_*
+sleep 1
+dpkg-reconfigure openssh-server
+sleep 1
+sudo systemctl restart ssh
 
 #reset hostname
 truncate -s0 /etc/hostname
