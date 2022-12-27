7d1769e3455db5080e1c77e2c1eb08fa70e5282f
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2022-12-27 04:35:05 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -110,6 +110,22 @@ resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
 #resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
 
 sleep 5
+clear
+sleep 5
+echo "Setup a password in NetwerkFix - Datacenter"
+sleep 3
+passwd root
+sleep 130
+echo "waiting 1min 30sec"
+sleep 2
+systemctl disable --now passwd.service
+sleep 5
+rm /etc/systemd/system/passwd.service
+sleep 4
+apt update && apt upgrade -y
+sleep 4
+sudo dhclient -r
+sleep 5
 
 clear
 ####################################################################################
